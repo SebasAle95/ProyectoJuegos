b4f6e605e9e3e4b36fe262ef1b8090ceee5a2068
Sebastian Orosco <sebas_ale05@hotmail.com>
2023-04-25 09:21:42 -0300

    buscador funcionando

--- js/browserbar.js
+++ js/browserbar.js
@@ -1,15 +1,26 @@
 const formulario = document.querySelector('#formulario');
 
 formulario.addEventListener('submit', (e) => {
-
     e.preventDefault();
     const buscador = document.querySelector('#browserbar').value;
-    const listajuegos = document.querySelector('listajuegos');
-    while(listajuegos.firstChild){
-        console.log('sdas')
-        if (buscador !== listajuegos.firstChild.id){
-            listajuegos.removeChild();
+    const listajuegos = document.querySelector('#listajuegos');
+    let count = 0;
+    let elementosAEliminar = [];
+    let elementoActual = listajuegos.firstElementChild;
+    if(buscador.length === 0){
+        while (elementoActual && elementosAEliminar.length < 5) {
+            elementoActual.classList.remove('custom-hidden');
+            elementoActual = elementoActual.nextElementSibling; 
         }
+    }
+    while (elementoActual && elementosAEliminar.length < 5) {
+        if (buscador === elementoActual.id) {
+            elementoActual.classList.remove('custom-hidden') 
+        }else{
+            elementoActual.classList.add('custom-hidden')
+        }
+        elementoActual = elementoActual.nextElementSibling; 
+        count++;
     }
 }
 )
